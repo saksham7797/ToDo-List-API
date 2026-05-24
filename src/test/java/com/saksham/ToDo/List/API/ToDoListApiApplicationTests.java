47b8d5641866de4e897c4b643930b83dfa28c37e
Saksham <sk3962364@gmail.com>
2026-05-24 11:38:20 +0530

    Application Initialization

---
+++ src/test/java/com/saksham/ToDo/List/API/ToDoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksham.ToDo.List.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
